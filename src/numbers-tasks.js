abdf08a0c705c688f82491edc19d3d4478a31447
egorReka <e.reka55@yandex.ru>
2024-11-11 22:56:53 +0300

    feat: add solutions getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -263,8 +263,26 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  let first = 0;
+  let second = 1;
+  let result;
+
+  if (index === first) {
+    return first;
+  }
+
+  if (index === second) {
+    return second;
+  }
+
+  for (let i = 1; i < index; i += 1) {
+    result = first + second;
+    first = second;
+    second = result;
+  }
+
+  return result;
 }
 
 /**
